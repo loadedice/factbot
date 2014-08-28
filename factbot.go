213a99b05e6aa8a2e316bdb33cb85a6c062912d2
joelina <loadedice@users.noreply.github.com>
2014-08-28 13:26:42 +1000

    Initial commit

---
+++ factbot.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+func DownloadURL(url string) string { //This function is taken from "Go-scrape"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(contents)
+}
+
+func deHTML(html string) string { //When/if I get this to work, i will make it another program. There is already a program called dehtml, but I haven't looked at how it works
+	return html
+}
+
+func main() {
+	URL := "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&prop=extracts&grnnamespace=0"
+	wikipediaRaw := DownloadURL(URL)
+	re := regexp.MustCompile(`(\"extract\"\:\")(.*?\.)`) //TODO: Parse this properly, without regular expressions. While it works for most of the cases I don't want to find one where it doesn't. And hey, it's JSON so yeah
+	fmt.Printf("%s\n", re.FindStringSubmatch(wikipediaRaw)[2])
+}
